0b0adb5a85905cf32aa261fe03c7429ba7fda019
pedrodruviaro <pedrodruviaro@gmail.com>
2025-04-16 19:52:46 -0300

    feat: add more dashboard components

--- src/pages/app/dashboard-page.tsx
+++ src/pages/app/dashboard-page.tsx
@@ -11,6 +11,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { MapPin, Pencil } from "lucide-react"
+import { Badge } from "@/components/ui/badge"
 
 export function DashboardPage() {
   return (
@@ -26,7 +39,7 @@ export function DashboardPage() {
 
             <Separator
               orientation="vertical"
-              style={{ height: "1.5rem", marginInline: "1rem" }}
+              style={{ height: "1.5rem", marginInline: ".5rem" }}
             />
 
             <DropdownMenu>
@@ -51,7 +64,109 @@ export function DashboardPage() {
           </div>
         </div>
       </header>
-      <main className="w-full max-w-6xl mx-auto px-6 my-10 lg:mt-16">main</main>
+
+      <div className="w-full max-w-6xl mx-auto px-6 my-10 lg:mt-16">
+        <main className="grid gap-10 lg:grid-cols-[2fr_1fr] lg:gap-6 lg:items-start">
+          <div className="space-y-10">
+            <section className="grid gap-6 md:grid-cols-2">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Total hours (month)</CardTitle>
+                  <CardDescription>
+                    <p className="text-xs">
+                      Lorem ipsum dolor sit amet, consectetur.
+                    </p>
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-2xl">20h</p>
+                </CardContent>
+              </Card>
+
+              <Card>
+                <CardHeader>
+                  <CardTitle>Activities (month)</CardTitle>
+                  <CardDescription>
+                    <p className="text-xs">
+                      Lorem ipsum dolor sit amet, consectetur.
+                    </p>
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-2xl">4</p>
+                </CardContent>
+              </Card>
+            </section>
+
+            <section>
+              <form className="space-y-4">
+                <div className="grid gap-2">
+                  <Label>Lorem</Label>
+                  <Input />
+                </div>
+
+                <div className="grid gap-4 md:grid-cols-2">
+                  <div className="grid gap-2">
+                    <Label>Lorem</Label>
+                    <Input />
+                  </div>
+                  <div className="grid gap-2">
+                    <Label>Lorem</Label>
+                    <Input />
+                  </div>
+                </div>
+
+                <Button>Create</Button>
+              </form>
+            </section>
+
+            <section className="border rounded-lg p-10 min-h-[400px]">
+              <p className="text-3xl">table</p>
+            </section>
+          </div>
+
+          <aside className="h-full">
+            <Card className="sticky top-6">
+              <CardHeader>
+                <div className="flex flex-col gap-4 md:flex-row md:items-center">
+                  <Avatar className="w-20 h-20">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+
+                  <div className="grid gap-2">
+                    <p className="font-semibold">John Doe</p>
+                    <p className="flex items-center gap-1 text-sm">
+                      <MapPin className="size-3" />
+                      <span>Porto Alegre, RS</span>
+                    </p>
+                  </div>
+                </div>
+              </CardHeader>
+
+              <CardContent>
+                <div className="grid gap-1">
+                  <Badge variant="outline" className="mb-2">
+                    Member since 20/20/20
+                  </Badge>
+                  <p>Lorem, ipsum.</p>
+                  <p className="text-xs">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Magni, eius?
+                  </p>
+                </div>
+              </CardContent>
+
+              <CardFooter>
+                <Button size="sm" variant="outline">
+                  <Pencil className="size-3" />
+                  Edit Profile
+                </Button>
+              </CardFooter>
+            </Card>
+          </aside>
+        </main>
+      </div>
     </div>
   )
 }
