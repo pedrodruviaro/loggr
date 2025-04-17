32b05b871ead699f12946b546ca5c79e757027b9
Pedro Henrique <pedrodruviaro@gmail.com>
2025-04-17 10:13:34 -0300

    feat: add tanstack react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from "@/components/theme/theme-provider"
 import { RouterProvider } from "react-router-dom"
 import { router } from "@/routes"
+import { queryClient } from "@/libs/query-client"
+import { QueryClientProvider } from "@tanstack/react-query"
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="loggr-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="loggr-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
